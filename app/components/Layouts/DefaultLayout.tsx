37592c661a3598e8a3b6d001d43baf4d826223ab
nmenag <nmena.garzon@gmail.com>
2024-04-18 17:45:16 -0500

    chore: remove checkbokes folder

--- app/components/Layouts/DefaultLayout.tsx
+++ app/components/Layouts/DefaultLayout.tsx
@@ -6,7 +6,7 @@ import Header from "@/app/components/Header";
 export default function DefaultLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: ReactNode;
 }) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   return (
